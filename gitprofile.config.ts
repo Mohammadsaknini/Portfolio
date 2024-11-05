7385c205713d5e881ac324b4d4e3d3a079a4f430
Mohammad Saknini <49996190+Mohammadsaknini@users.noreply.github.com>
2024-11-05 20:52:05 +0100

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -2,7 +2,7 @@
 
 const CONFIG = {
   github: {
-    username: 'MohammadSakhnini', // Your GitHub org/user name. (This is the only required config)
+    username: 'MohammadSaknini', // Your GitHub org/user name. (This is the only required config)
   },
   /**
    * If you are deploying to https://<USERNAME>.github.io/, for example your repository is at https://github.com/arifszn/arifszn.github.io, set base to '/'.
