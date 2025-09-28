b06018d42b74c49ef574249d3927a61918a418d4
Mohammad Saknini <49996190+Mohammadsaknini@users.noreply.github.com>
2025-09-28 14:07:36 +0200

    Fix PaperSeek report link and title formatting
    Updated the link for the PaperSeek report and corrected the title formatting.

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -172,12 +172,12 @@ const CONFIG = {
         techniques based on a varity of features that take into considration not only the textual content but
         rather the structure and history of the article.`
     },
-      {
+    {
       title: 'Semantic Embeddings  for Literature Discovery: An AI-Based Alternative to Query-Based Search',
       conferenceName: '',
       journalName: '',
       authors: 'Mohammad Saknini',
-      link: 'https://github.com/Mohammadsaknini/WikipediaTools/blob/main/Thesis.pdf',
+      link: 'https://github.com/Mohammadsaknini/PaperSeek-Report/blob/main/report.pdf',
       description:
         `A semantic search framework (PaperSeek) designed to enhance large-scale literature discovery by using research questions,
          domain-relevant context, synthetic document generation, and active learning-based reranking. Evaluations have shown that PaperSeek
